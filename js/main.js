1bf0cbe180f7d22ef546bb630354a31c2e55813d
caveeagle <a@a.ru>
2015-03-03 00:45:09 +0300

    Add demo for blocks

--- js/main.js
+++ js/main.js
@@ -2,5 +2,13 @@
 function main()
 {
     
-    $('body').append('<br/>Этот текст добавлен с помощью jQuery');
+    var wallBlock = '\u2592';
+    
+    var runnerBlock = '\u03A9';
+    
+    
+    $("#labfield tr td").eq(6).text(wallBlock);
+
+    $("#labfield tr td").eq(5).text(runnerBlock);
+
 }
